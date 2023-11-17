44f41b7f877cc0f1dd3c31faab1297465afcf951
dkimb0 <jhdavidk@gmail.com>
2023-11-17 16:43:01 -0500

    attempt using html loader to load images

--- webpack.config.js
+++ webpack.config.js
@@ -17,8 +17,10 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
+        // test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        // type: "asset/resource",
+        test: /\.(html)$/,
+        use: ['html-loader'],
       },
     ],
   },
